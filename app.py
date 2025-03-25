5c71e61e9a33239f1aa941a0d778960ed7130bdb
Al Ilseman <alexander.ilseman@gmail.com>
2025-03-24 20:44:16 -0500

    Good baseline, need a break

--- app.py
+++ app.py
@@ -1,11 +1,48 @@
-from tkinter import Tk, Canvas
+import tkinter as tk
 
-root = Tk()
-canvas = Canvas(root, width=500, height=500)
+root = tk.Tk()
+canvas = tk.Canvas(root, width=500, height=500, bg="#eeeeee")
 canvas.pack()
 
-points = [30, 40, 40, 35, 50, 40, 40, 20]
 
-canvas.create_polygon(points, fill="black")
+# NOTE: I should seperate the converns of drawing boid glyphs from regular point geometry drawing
+# A boid should have a circle that acts as its boundries and position, etc. then the triangle/cheveron
+# should be drawn in the center of the circle 
+def rotate(boid):
+    # Bunch of trigonometry
+    return False
 
+
+def drawBoid(boid, canvas):
+    # For a boid with position=(x,y)
+    # [10+x, 20+y, 20+x... ]
+    dx = boid["position"]["x"]
+    dy = boid["position"]["y"]
+    points = [10 + dx, 20 + dy, 20 + dx, 15 + dy, 30 + dx, 20 + dy, 20 + dx, dy]
+    # Well that's cool and all, but what about rotation?
+    canvas.create_polygon(points, fill="black")
+
+
+
+class Vector(object):
+
+    def __init__(self, x=0, y=0, z=0):
+        self._x, self._y, self._z = x, y, z
+
+    def setx(self, x): self._x = float(x)
+    def sety(self, y): self._y = float(y)        
+    def setz(self, z): self._z = float(z)     
+
+    x = property(lambda self: float(self._x), setx)
+    y = property(lambda self: float(self._y), sety)
+    z = property(lambda self: float(self._z), setz)
+
+class Boid:
+    __init__(self, init_pos):
+        self.position = Vector(*init_pos)
+
+# boid_o.position.x
+# boid = {"position": {"x": 10, "y": 15}}
+
+drawBoid(boid, canvas)
 root.mainloop()
