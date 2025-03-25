48f34af3e9e9412b9067957e91389b81bd026fe8
Al Ilseman <alexander.ilseman@gmail.com>
2025-03-24 20:48:38 -0500

    Fix typeo; take break

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ canvas.pack()
 
 # NOTE: I should seperate the converns of drawing boid glyphs from regular point geometry drawing
 # A boid should have a circle that acts as its boundries and position, etc. then the triangle/cheveron
-# should be drawn in the center of the circle 
+# should be drawn in the center of the circle
 def rotate(boid):
     # Bunch of trigonometry
     return False
@@ -23,26 +23,32 @@ def drawBoid(boid, canvas):
     canvas.create_polygon(points, fill="black")
 
 
-
 class Vector(object):
 
     def __init__(self, x=0, y=0, z=0):
         self._x, self._y, self._z = x, y, z
 
-    def setx(self, x): self._x = float(x)
-    def sety(self, y): self._y = float(y)        
-    def setz(self, z): self._z = float(z)     
+    def setx(self, x):
+        self._x = float(x)
+
+    def sety(self, y):
+        self._y = float(y)
+
+    def setz(self, z):
+        self._z = float(z)
 
     x = property(lambda self: float(self._x), setx)
     y = property(lambda self: float(self._y), sety)
     z = property(lambda self: float(self._z), setz)
 
+
 class Boid:
-    __init__(self, init_pos):
+    def __init__(self, init_pos):
         self.position = Vector(*init_pos)
+
 
 # boid_o.position.x
 # boid = {"position": {"x": 10, "y": 15}}
 
-drawBoid(boid, canvas)
+# drawBoid(boid, canvas)
 root.mainloop()
