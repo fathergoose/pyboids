6a4f64c872b9f069bd857889de69a438ee9daa78
Alexander Ilseman <alexander.ilseman.com>
2025-03-24 22:30:11 -0500

    Refined approach

--- app.py
+++ app.py
@@ -1,30 +1,7 @@
 import tkinter as tk
 
-root = tk.Tk()
-canvas = tk.Canvas(root, width=500, height=500, bg="#eeeeee")
-canvas.pack()
-
-
-# NOTE: I should seperate the converns of drawing boid glyphs from regular point geometry drawing
-# A boid should have a circle that acts as its boundries and position, etc. then the triangle/cheveron
-# should be drawn in the center of the circle
-def rotate(boid):
-    # Bunch of trigonometry
-    return False
-
-
-def drawBoid(boid, canvas):
-    # For a boid with position=(x,y)
-    # [10+x, 20+y, 20+x... ]
-    dx = boid["position"]["x"]
-    dy = boid["position"]["y"]
-    points = [10 + dx, 20 + dy, 20 + dx, 15 + dy, 30 + dx, 20 + dy, 20 + dx, dy]
-    # Well that's cool and all, but what about rotation?
-    canvas.create_polygon(points, fill="black")
-
 
 class Vector(object):
-
     def __init__(self, x=0, y=0, z=0):
         self._x, self._y, self._z = x, y, z
 
@@ -43,12 +20,23 @@ class Vector(object):
 
 
 class Boid:
-    def __init__(self, init_pos):
-        self.position = Vector(*init_pos)
+    _base_points = [0, 20, 10, 15, 20, 20, 10, 0]
+    _center = [10, 15]
+    _point_offsets = [-10, 5, 0, 0, 10, 5, 0, -15]
+    radius = 10
+
+    def __init__(self, position, speed, heading):
+        self.position = Vector(*position)
+        self.speed = speed
+        self.heading = heading
+
+    def points(self):
+        return "translate center of mass into a polygon"
 
 
-# boid_o.position.x
-# boid = {"position": {"x": 10, "y": 15}}
+root = tk.Tk()
+canvas = tk.Canvas(root, width=500, height=500, bg="#eeeeee")
+canvas.pack()
+canvas.create_polygon([0, 20, 10, 15, 20, 20, 10, 0])
 
-# drawBoid(boid, canvas)
 root.mainloop()
